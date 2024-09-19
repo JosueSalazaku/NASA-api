d3b60501255cc7058d04415877717438aefea677
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-20 01:30:47 +0200

    Display NASA Image of the Day

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,55 @@
-import { resolvePath } from 'react-router';
 import './style.css'
 
-const displaySection = document.querySelector('.display-section');
-const imageDisplay = document.querySelector('.image-display');
+interface NasaApodData {
+  copyright?: string;        
+  date: string;             
+  explanation: string;      
+  hdurl?: string;            
+  media_type: string;       
+  service_version: string;  
+  title: string;             
+  url: string;             
+}
 
+const displaySection = document.querySelector('#display-section');
 const apiKey = import.meta.env.VITE_NASA_API_KEY;
-
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
 
 async function fetchImageOfTheDay() {
   try {
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+    const response = await fetch(url);
     if (!response.ok) {
-      throw new Error(`Response statuss: ${response.status}`)
+      throw new Error(`Response statuss: ${response.status}`) 
     } 
 
     const data = await response.json()
     console.log(data);
+    return data
   } catch (error) {
     console.error(error, 'Data not found');
-    
   }
 }
 
-fetchImageOfTheDay()
+async function displayImageOfTheDay(data: NasaApodData) {
+  if (!data) {
+    console.error('No data to display');
+    return;
+  }
+
+  const imageOfTheDay = document.createElement("img");
+  displaySection?.appendChild(imageOfTheDay) 
+  imageOfTheDay.src = data.url
+  imageOfTheDay.alt = data.title
+  displaySection?.appendChild(imageOfTheDay);
+
+}
+
+
+async function init() {
+  const data = await fetchImageOfTheDay(); 
+  if (data) {
+    displayImageOfTheDay(data); 
+  }
+}
+
+init(); 
